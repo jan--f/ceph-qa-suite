160686d63f4a40ae0e67f998ff97061f67057745
Greg Farnum <gfarnum@redhat.com>
2014-11-06 16:13:39 -0800

    Fixup "mds_scrub_checks: This task runs scrub and flush commands on the MDS."
    Follow PEP8 whitespace conventions.
    
    Signed-off-by: Greg Farnum <gfarnum@redhat.com>

--- tasks/mds_scrub_checks.py
+++ tasks/mds_scrub_checks.py
@@ -9,6 +9,7 @@ from teuthology import misc as teuthology
 
 log = logging.getLogger(__name__)
 
+
 def task(ctx, config):
     """
     Run flush and scrub commands on the specified files in the filesystem. This
@@ -25,8 +26,8 @@ def task(ctx, config):
       client: 0
       run_seq: [0-9]+
 
-    Increment the run_seq on subsequent invocations within a single test run; it
-    uses that value to generate unique folder and file names.
+    Increment the run_seq on subsequent invocations within a single test run;
+    it uses that value to generate unique folder and file names.
     """
 
     mds_id = config.get("mds_id")
@@ -35,36 +36,41 @@ def task(ctx, config):
     client_id = config.get("client")
 
     if mds_id is None or test_path is None or run_seq is None:
-        raise ValueError("Must specify each of mds_id, test_path, run_seq, client_id in config!")
+        raise ValueError("Must specify each of mds_id, test_path, run_seq,"
+                         "client_id in config!")
 
     teuthdir = teuthology.get_testdir(ctx)
-    client_path = "{teuthdir}/mnt.{id_}/{test_path}".format(teuthdir=teuthdir,
-                                                            id_=client_id,
-                                                            test_path=test_path)
+    client_path = "{teuthdir}/mnt.{id_}/{test_path}".\
+                  format(teuthdir=teuthdir,
+                         id_=client_id,
+                         test_path=test_path)
 
     log.info("Cloning repo into place (if not present)")
     repo_path = clone_repo(ctx, client_id, client_path)
 
-    log.info("Initiating mds_scrub_checks on mds.{id_}, test_path {path}, run_seq {seq}".format(
-            id_=mds_id, path=test_path, seq=run_seq))
+    log.info("Initiating mds_scrub_checks on mds.{id_}, "
+             "test_path {path}, run_seq {seq}".format(
+                 id_=mds_id, path=test_path, seq=run_seq))
 
-    def json_validator(json,rc,element,expected_value):
+    def json_validator(json, rc, element, expected_value):
         if (rc != 0):
             return False, "asok command returned error {rc}".format(rc=str(rc))
         element_value = json.get(element)
         if element_value != expected_value:
             return False, "unexpectedly got {jv} instead of {ev}!".format(
-                jv=element_value,ev=expected_value)
+                jv=element_value, ev=expected_value)
         return True, "Succeeded"
 
-    success_validator = lambda j,r: json_validator(j,r,"return_code",0)
+    success_validator = lambda j, r: json_validator(j, r, "return_code", 0)
 
     nep = "{test_path}/i/dont/exist".format(test_path=test_path)
     command = "flush_path {nep}".format(nep=nep)
-    asok_command(ctx, mds_id, command, lambda j,r: json_validator(j,r,"return_code",-2))
-    
+    asok_command(ctx, mds_id, command,
+                 lambda j, r: json_validator(j, r, "return_code", -2))
+
     command = "scrub_path {nep}".format(nep=nep)
-    asok_command(ctx, mds_id, command, lambda j,r: json_validator(j,r,"return_code",-2))
+    asok_command(ctx, mds_id, command,
+                 lambda j, r: json_validator(j, r, "return_code", -2))
 
     test_repo_path = "{test_path}/ceph-qa-suite".format(test_path=test_path)
     dirpath = "{repo_path}/suites".format(repo_path=test_repo_path)
@@ -85,11 +91,13 @@ def task(ctx, config):
     command = "scrub_path {filepath}".format(filepath=filepath)
     asok_command(ctx, mds_id, command, success_validator)
 
-    filepath = "{repo_path}/suites/fs/basic/clusters/fixed-3-cephfs.yaml".format(
-        repo_path=test_repo_path)
+    filepath = "{repo_path}/suites/fs/basic/clusters/fixed-3-cephfs.yaml".\
+               format(repo_path=test_repo_path)
     command = "scrub_path {filepath}".format(filepath=filepath)
-    asok_command(ctx, mds_id, command, lambda j,r: json_validator(j,r,"performed_validation",False))
-    
+    asok_command(ctx, mds_id, command,
+                 lambda j, r: json_validator(j, r, "performed_validation",
+                                             False))
+
     if (run_seq == 0):
         log.info("First run: flushing base dir /")
         command = "flush_path /"
@@ -98,26 +106,31 @@ def task(ctx, config):
     asok_command(ctx, mds_id, command, success_validator)
 
     client = ctx.manager.find_remote("client", client_id)
-    new_dir = "{repo_path}/new_dir_{i}".format(repo_path=repo_path,i=run_seq)
-    test_new_dir = "{repo_path}/new_dir_{i}".format(repo_path=test_repo_path,i=run_seq)
+    new_dir = "{repo_path}/new_dir_{i}".format(repo_path=repo_path, i=run_seq)
+    test_new_dir = "{repo_path}/new_dir_{i}".format(repo_path=test_repo_path,
+                                                    i=run_seq)
     client.run(args=[
-            "mkdir", new_dir])
+        "mkdir", new_dir])
     command = "flush_path {dir}".format(dir=test_new_dir)
     asok_command(ctx, mds_id, command, success_validator)
 
-    new_file = "{repo_path}/new_file_{i}".format(repo_path=repo_path,i=run_seq)
-    test_new_file = "{repo_path}/new_file_{i}".format(repo_path=test_repo_path,i=run_seq)
+    new_file = "{repo_path}/new_file_{i}".format(repo_path=repo_path,
+                                                 i=run_seq)
+    test_new_file = "{repo_path}/new_file_{i}".format(repo_path=test_repo_path,
+                                                      i=run_seq)
     client.run(args=[
-            "echo", "hello", run.Raw('>'), new_file])
+        "echo", "hello", run.Raw('>'), new_file])
     command = "flush_path {file}".format(file=test_new_file)
     asok_command(ctx, mds_id, command, success_validator)
 
     command = "flush_path /"
     asok_command(ctx, mds_id, command, success_validator)
-    
+
+
 class AsokCommandFailedError(Exception):
     """
-    Exception thrown when we get an unexpected response on an admin socket command
+    Exception thrown when we get an unexpected response
+    on an admin socket command
     """
     def __init__(self, command, rc, json, errstring):
         self.command = command
@@ -126,16 +139,19 @@ class AsokCommandFailedError(Exception):
         self.errstring = errstring
 
     def __str__(self):
-        return "Admin socket: {command} failed with rc={rc},json output={json}, because '{es}'".format(
-            command=self.command,rc=self.rc,json=self.json,es=self.errstring)
-        
+        return "Admin socket: {command} failed with rc={rc},"
+        "json output={json}, because '{es}'".format(
+            command=self.command, rc=self.rc,
+            json=self.json, es=self.errstring)
+
 
 def asok_command(ctx, mds_id, command, validator):
     log.info("Running command '{command}'".format(command=command))
-    
+
     command_list = command.split()
 
-    proc = ctx.manager.admin_socket('mds', mds_id, command_list, check_status=False)
+    proc = ctx.manager.admin_socket('mds', mds_id,
+                                    command_list, check_status=False)
     rout = proc.exitstatus
     sout = proc.stdout.getvalue()
 
@@ -144,19 +160,21 @@ def asok_command(ctx, mds_id, command, validator):
     else:
         jout = None
 
-    log.info("command '{command}' got response code '{rout}' and stdout '{sout}'".format(
-            command=command,rout=rout,sout=sout))
+    log.info("command '{command}' got response code "
+             "'{rout}' and stdout '{sout}'".format(
+                 command=command, rout=rout, sout=sout))
 
-    success,errstring = validator(jout,rout)
+    success, errstring = validator(jout, rout)
 
     if not success:
         raise AsokCommandFailedError(command, rout, jout, errstring)
 
     return jout
 
+
 def clone_repo(ctx, client_id, path):
     repo = "ceph-qa-suite"
-    repo_path = "{path}/{repo}".format(path=path,repo=repo)
+    repo_path = "{path}/{repo}".format(path=path, repo=repo)
 
     client = ctx.manager.find_remote("client", client_id)
     client.run(
@@ -169,7 +187,7 @@ def clone_repo(ctx, client_id, path):
             "ls", repo_path, run.Raw('||'),
             "git", "clone", '--branch', 'giant',
             "http://github.com/ceph/{repo}".format(repo=repo),
-            "{path}/{repo}".format(path=path,repo=repo)
+            "{path}/{repo}".format(path=path, repo=repo)
             ]
         )
 
